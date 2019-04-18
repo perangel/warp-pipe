c46d74ecf94c92c3067197e88a5ed782966ce2b6
Angel Perez <perangel@gmail.com>
2019-04-18 09:16:03 -0400

    improve: pipeline API

--- pkg/pipeline/pipeline.go
+++ pkg/pipeline/pipeline.go
@@ -6,12 +6,47 @@ import (
 	"github.com/perangel/warp-pipe/pkg/model"
 )
 
-type StageFn func(context.Context, <-chan *model.Changeset, chan error) <-chan *model.Changeset
+type stageFn func(context.Context, <-chan *model.Changeset, chan error) <-chan *model.Changeset
+
+func wrapStageFunc(sFun StageFunc) stageFn {
+	f := func(ctx context.Context, inCh <-chan *model.Changeset, errCh chan error) <-chan *model.Changeset {
+		outCh := make(chan *model.Changeset)
+		go func() {
+			defer close(outCh)
+			for {
+				select {
+				case change := <-inCh:
+					c, err := sFun(change)
+					if err != nil {
+						errCh <- err
+					}
+
+					if c == nil {
+						continue
+					}
+
+					outCh <- c
+				case <-ctx.Done():
+					return
+				}
+			}
+		}()
+		return outCh
+	}
+	return f
+}
+
+// StageFunc is a function for processing changesets in a Pipline Stage.
+// It accepts a single argument, a Changset, and returns one of:
+//     (Changset, nil): If the stage was successful
+//     (nil, nil): If the changeset should be dropped (useful for filtering)
+//     (nil, error): If there was an error during the stage
+type StageFunc func(*model.Changeset) (*model.Changeset, error)
 
 // Stage is a pipeline stage.
 type Stage struct {
 	Name string
-	Fn   StageFn
+	Fn   stageFn
 }
 
 // Pipeline represents a sequence of stages for processing Changesets.
@@ -29,10 +64,10 @@ func NewPipeline() *Pipeline {
 	}
 }
 
-func (p *Pipeline) AddStage(name string, fn StageFn) {
+func (p *Pipeline) AddStage(name string, fn StageFunc) {
 	p.Stages = append(p.Stages, &Stage{
 		Name: name,
-		Fn:   fn,
+		Fn:   wrapStageFunc(fn),
 	})
 }
 
--- pkg/pipeline/pipeline_test.go
+++ pkg/pipeline/pipeline_test.go
@@ -3,8 +3,8 @@ package pipeline
 import (
 	"context"
 	"strings"
-	"sync"
 	"testing"
+	"time"
 
 	"github.com/perangel/warp-pipe/pkg/model"
 	"github.com/stretchr/testify/assert"
@@ -13,62 +13,32 @@ import (
 func TestPipeline(t *testing.T) {
 	p := NewPipeline()
 
-	p.AddStage("remove_pii", func(ctx context.Context, inCh <-chan *model.Changeset, errCh chan error) <-chan *model.Changeset {
-		outCh := make(chan *model.Changeset)
-		go func() {
-			defer close(outCh)
-			for {
-				select {
-				case change := <-inCh:
-					if _, ok := change.NewValues["first_name"]; ok {
-						delete(change.NewValues, "first_name")
-					}
-					outCh <- change
-				case <-ctx.Done():
-					return
-				}
-			}
-		}()
-		return outCh
+	p.AddStage("remove_pii", func(change *model.Changeset) (*model.Changeset, error) {
+		if _, ok := change.NewValues["first_name"]; ok {
+			delete(change.NewValues, "first_name")
+		}
+		return change, nil
 	})
 
-	p.AddStage("uppercase_tablename", func(ctx context.Context, inCh <-chan *model.Changeset, errCh chan error) <-chan *model.Changeset {
-		outCh := make(chan *model.Changeset)
-		go func() {
-			defer close(outCh)
-			for {
-				select {
-				case change := <-inCh:
-					change.Table = strings.ToUpper(change.Table)
-					outCh <- change
-				case <-ctx.Done():
-					return
-				}
-			}
-		}()
-		return outCh
-
+	p.AddStage("uppercase_tablename", func(change *model.Changeset) (*model.Changeset, error) {
+		change.Table = strings.ToUpper(change.Table)
+		return change, nil
 	})
 
-	var wg sync.WaitGroup
-
-	sourceCh := make(chan *model.Changeset)
-	outCh, errCh := p.Start(context.Background(), sourceCh)
-	go func() {
-		for {
-			select {
-			case msg := <-outCh:
-				assert.NotContains(t, msg.NewValues, "first_name")
-				assert.Equal(t, "USERS", msg.Table)
-				wg.Done()
-			case err := <-errCh:
-				t.Error(err)
-				t.FailNow()
+	p.AddStage("filter_test_users", func(change *model.Changeset) (*model.Changeset, error) {
+		if isTest, ok := change.NewValues["is_test"]; ok {
+			if isTest.Value == "TRUE" {
+				return nil, nil
 			}
 		}
-	}()
+		return change, nil
+	})
 
-	changeSetWithPii := &model.Changeset{
+	sourceCh := make(chan *model.Changeset)
+	ctx, cancel := context.WithCancel(context.Background())
+	outCh, errCh := p.Start(ctx, sourceCh)
+
+	changesetWithPii := &model.Changeset{
 		Table: "users",
 		NewValues: map[string]*model.ChangesetColumn{
 			"first_name": &model.ChangesetColumn{
@@ -79,8 +49,42 @@ func TestPipeline(t *testing.T) {
 		},
 	}
 
-	wg.Add(1)
-	sourceCh <- changeSetWithPii
+	changesetForTestUser := &model.Changeset{
+		Table: "users",
+		NewValues: map[string]*model.ChangesetColumn{
+			"first_name": &model.ChangesetColumn{
+				Name:  "first_name",
+				Type:  "string",
+				Value: "Alice",
+			},
+			"is_test": &model.ChangesetColumn{
+				Name:  "is_test",
+				Type:  "boolean",
+				Value: "TRUE",
+			},
+		},
+	}
 
-	wg.Wait()
+	// NOTE: only add 1 to the waitgroup since the test user will be dropped
+	for _, change := range []*model.Changeset{changesetWithPii, changesetForTestUser} {
+		sourceCh <- change
+	}
+
+	var results []*model.Changeset
+	go func() {
+		for {
+			select {
+			case change := <-outCh:
+				results = append(results, change)
+			case err := <-errCh:
+				cancel()
+				t.Error(err)
+			}
+		}
+	}()
+
+	_ = <-time.After(300 * time.Millisecond)
+	assert.Equal(t, 1, len(results))
+	assert.NotContains(t, results[0].NewValues, "first_name")
+	assert.Equal(t, "USERS", results[0].Table)
 }
