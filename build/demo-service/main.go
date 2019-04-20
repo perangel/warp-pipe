a8c0999e9a15834da2ac6179e60efff7a37ef9d1
Angel Perez <perangel@gmail.com>
2019-04-19 22:17:52 -0400

    feature: basic demo

---
+++ build/demo-service/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/perangel/warp-pipe/pkg/warp-pipe"
+
+	"gopkg.in/olahol/melody.v1"
+)
+
+var M = melody.New()
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+func WebSocketHandler(w http.ResponseWriter, r *http.Request) {
+	M.HandleRequest(w, r)
+}
+
+func WebSocketMessageHandler(s *melody.Session, msg []byte) {
+	M.Broadcast(msg)
+}
+
+func main() {
+	http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/ws", WebSocketHandler)
+	M.HandleMessage(WebSocketMessageHandler)
+
+	wpConfig := warppipe.NewConfigFromEnv()
+	wpConfig.ListenerType = warppipe.ListenerTypeLogicalReplication
+	wp := warppipe.NewWarpPipe(wpConfig)
+	err := wp.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	changes, _ := wp.ListenForChanges(context.Background())
+	go func() {
+		for change := range changes {
+			b, _ := json.Marshal(change)
+			M.Broadcast(b)
+		}
+	}()
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
