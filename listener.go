026d090c58d1536178212a4ee3fbefb428a1ac52
Angel Perez <perangel@gmail.com>
2019-05-10 14:54:49 -0400

    feat: PersistentListener interface

--- listener.go
+++ listener.go
@@ -12,3 +12,10 @@ type Listener interface {
 	ListenForChanges(context.Context) (chan *Changeset, chan error)
 	Close() error
 }
+
+// PersistentListener is an interface for implementing a changeset listener that
+// can persist its state.
+type PersistentListener interface {
+	Listener
+	CommitState(context.Context) error
+}
