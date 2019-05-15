c76b1d0532859e57b60893648023275b2400af6e
Angel Perez <perangel@gmail.com>
2019-05-15 16:03:48 -0400

    feat: teardown-db command

--- internal/cli/db.go
+++ internal/cli/db.go
@@ -1,6 +1,8 @@
 package cli
 
 import (
+	"fmt"
+
 	"github.com/jackc/pgx"
 	"github.com/perangel/warp-pipe/internal/db"
 	"github.com/spf13/cobra"
@@ -20,10 +22,10 @@ var setupDBCmd = &cobra.Command{
 	Long: `Setup the source database for tracking changesets.
 
 This command adds a new 'warp_pipe' schema with a 'changesets' table to the source
-database, and registers a TRIGGER that will write all table changes after INSERT,
+database, and registers a trigger that will write all table changes after INSERT,
 UPDATE, or DELETE to the 'warp_pipe.changesets' table.
 
-Once this is setup, you can run 'warp-pipe' with the 'queue' listener to stream
+Once this is setup, you can run 'warp-pipe' with the 'audit' listener to stream
 the changesets.
 
 For more details see: https://github.com/perangel/warp-pipe/docs/setup_database.md
@@ -52,6 +54,40 @@ For more details see: https://github.com/perangel/warp-pipe/docs/setup_database.
 			return err
 		}
 
+		fmt.Println("Successfully created `warp_pipe` schema")
+		return nil
+	},
+}
+
+var teardownDBCmd = &cobra.Command{
+	Use:   "teardown-db",
+	Short: "Teardown the `warp_pipe` schema ",
+	Long:  `Teardown the 'warp_pipe' schema in the source database.`,
+	RunE: func(cmd *cobra.Command, _ []string) error {
+		config, err := parseConfig()
+		if err != nil {
+			return err
+		}
+
+		dbConfig := &pgx.ConnConfig{
+			Host:     config.Database.Host,
+			Port:     uint16(config.Database.Port),
+			User:     config.Database.User,
+			Password: config.Database.Password,
+			Database: config.Database.Database,
+		}
+
+		conn, err := pgx.Connect(*dbConfig)
+		if err != nil {
+			return err
+		}
+
+		err = db.Teardown(conn)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("Successfully removed `warp_pipe` schema")
 		return nil
 	},
 }
