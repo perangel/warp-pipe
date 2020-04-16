f06083a6847ac51fd00cd39715955a5010602b4e
Brad Erickson <bderickson@gmail.com>
2020-04-15 19:27:29 -0700

    feat: Support JSON/B fields in Changesets
    JSON/B fields are stored in the NewValue ChangesetColumn as a
    map[string]interface {} which the "sql" package does not support.
    Solution: Attempt to convert the map to a string or Fatal.

--- cmd/axon/sql.go
+++ cmd/axon/sql.go
@@ -1,24 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"strings"
+
+	"reflect"
 
 	"github.com/jmoiron/sqlx"
 	warppipe "github.com/perangel/warp-pipe"
 )
 
-func prepareQueryArgs(changesetCols []*warppipe.ChangesetColumn) ([]string, []string, map[string]interface{}) {
+func prepareQueryArgs(changesetCols []*warppipe.ChangesetColumn) ([]string, []string, map[string]interface{}, error) {
 	var cols []string
 	var colArgs []string
 	values := make(map[string]interface{}, len(cols))
 	for _, v := range changesetCols {
+		t := reflect.TypeOf(v.Value)
+		if t != nil && t.Kind() == reflect.Map {
+			// Found a hashmap, this is a JSON/B field. Convert manually to string to
+			// avoid package "sql" error: "unsupported type map[string]interface {}"".
+			b, err := json.Marshal(v.Value)
+			if err != nil {
+				return cols, colArgs, values, fmt.Errorf("unable to marshal JSON field %s: %w", v.Column, err)
+			}
+			v.Value = string(b)
+		}
 		cols = append(cols, v.Column)
 		colArgs = append(colArgs, fmt.Sprintf(":%s", v.Column))
 		values[v.Column] = v.Value
 	}
 
-	return cols, colArgs, values
+	return cols, colArgs, values, nil
 }
 
 func preparePrimaryKeyWhereClause(table string, primaryKey []string) string {
@@ -31,7 +45,12 @@ func preparePrimaryKeyWhereClause(table string, primaryKey []string) string {
 }
 
 func prepareInsertQuery(schema string, change *warppipe.Changeset) (string, map[string]interface{}) {
-	cols, colArgs, values := prepareQueryArgs(change.NewValues)
+	cols, colArgs, values, err := prepareQueryArgs(change.NewValues)
+	if err != nil {
+		// TODO: Is failure the best option here? Probably no way to safely save anything.
+		log.Fatalf("prepareQueryArgs: error in changeset %s: %s", change, err)
+	}
+
 	sql := fmt.Sprintf(
 		"INSERT INTO %s.%s (%s) VALUES (%s)",
 		schema,
@@ -44,7 +63,10 @@ func prepareInsertQuery(schema string, change *warppipe.Changeset) (string, map[
 }
 
 func prepareUpdateQuery(schema string, primaryKey []string, change *warppipe.Changeset) (string, map[string]interface{}) {
-	cols, colArgs, values := prepareQueryArgs(change.NewValues)
+	cols, colArgs, values, err := prepareQueryArgs(change.NewValues)
+	if err != nil {
+		log.Fatalf("prepareQueryArgs: error in changeset %s: %s", change, err)
+	}
 	setClauses := make([]string, len(cols))
 	for i, c := range cols {
 		setClauses[i] = fmt.Sprintf("%s = :%s", c, c)
@@ -72,7 +94,10 @@ func prepareUpdateQuery(schema string, primaryKey []string, change *warppipe.Cha
 }
 
 func prepareDeleteQuery(schema string, primaryKey []string, change *warppipe.Changeset) (string, map[string]interface{}) {
-	_, _, values := prepareQueryArgs(change.OldValues)
+	_, _, values, err := prepareQueryArgs(change.NewValues)
+	if err != nil {
+		log.Fatalf("prepareQueryArgs: error in changeset %s: %s", change, err)
+	}
 
 	sql := fmt.Sprintf(
 		"DELETE FROM %s.%s WHERE %s",
@@ -88,8 +113,9 @@ func insertRow(conn *sqlx.DB, schema string, change *warppipe.Changeset) error {
 	query, args := prepareInsertQuery(schema, change)
 	_, err := conn.NamedExec(query, args)
 	if err != nil {
-		return fmt.Errorf("Error: failed to insert: %v", err)
+		return fmt.Errorf("failed to insert %s: %w", change, err)
 	}
+	log.Printf("row inserted: %s", change)
 	return nil
 }
 
@@ -99,6 +125,7 @@ func updateRow(conn *sqlx.DB, schema string, change *warppipe.Changeset, primary
 	if err != nil {
 		return fmt.Errorf("Error: failed to update row: %v", err)
 	}
+	log.Printf("row updated: %s", change)
 	return nil
 }
 
@@ -108,5 +135,6 @@ func deleteRow(conn *sqlx.DB, schema string, change *warppipe.Changeset, primary
 	if err != nil {
 		return fmt.Errorf("Error: failed to delete row: %v", err)
 	}
+	log.Printf("row deleted: %s", change)
 	return nil
 }
