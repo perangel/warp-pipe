b375b66c40eda693853f0728ca94b35aabecd510
Brad Erickson <bderickson@gmail.com>
2020-04-17 23:54:48 -0700

    feat: Update SERIAL sequence after INSERT queries
    Sequence values are not updated when a row is inserted including the
    sequenced value, often the ID.
    
    Checks for sequenced columns on startup and then runs setval() after
    each insert to manually update the value for that column.

--- cmd/axon/schema.go
+++ cmd/axon/schema.go
@@ -14,6 +14,9 @@ import (
 // maps primary key columns by table
 var primaryKeys = make(map[string][]string)
 
+// maps serial key columns by table
+var columnSequences = make(map[string]string)
+
 func checkTargetVersion(conn *sqlx.DB) error {
 	var serverVersion string
 	err := conn.QueryRow("SHOW server_version;").Scan(&serverVersion)
@@ -72,4 +75,74 @@ func getPrimaryKeyForChange(change *warppipe.Changeset) ([]string, error) {
 		return nil, fmt.Errorf("no primary key in mapping for table `%s`", change.Table)
 	}
 	return col, nil
+}
+
+// loadColumnSequences loads sequences used explictly in a table column which
+// need to be updated after INSERTs.
+func loadColumnSequences(conn *sqlx.DB) error {
+	var rows []struct {
+		TableName     string `db:"table_name"`
+		ColumnName    string `db:"column_name"`
+		ColumnDefault string `db:"column_default"`
+	}
+	err := conn.Select(&rows, `
+	SELECT
+		table_name,
+		column_name,
+		column_default
+	FROM information_schema.columns
+	WHERE
+		table_schema = 'public'
+	AND
+		column_default LIKE 'nextval(%'`,
+	)
+	if err != nil {
+		return fmt.Errorf("loadColumnSequences: %w", err)
+	}
+
+	for _, r := range rows {
+		colDefault := strings.Split(r.ColumnDefault, "'")
+		sequenceName := colDefault[1]
+
+		columnSequences[r.TableName+"/"+r.ColumnName] = sequenceName
+	}
+	log.Printf("column sequences found: %v", columnSequences)
+	return nil
+}
+
+func getSequenceColumn(table, column string) (string, bool) {
+	if sequenceName, ok := columnSequences[table+"/"+column]; ok {
+		return sequenceName, true
+	}
+	return "", false
+}
+
+func updateColumnSequence(conn *sqlx.DB, table string, columns []*warppipe.ChangesetColumn) error {
+	// Why no transaction? From the manual: Because sequences are
+	// non-transactional, changes made by setval are not undone if the transaction
+	// rolls back.
+	// https://www.postgresql.org/docs/9.6/functions-sequence.html
+	for _, c := range columns {
+		sequenceName, ok := getSequenceColumn(table, c.Column)
+		if !ok {
+			// Column does not have a SERIAL sequence
+			continue
+		}
+		var rows []struct {
+			SetVal string `db:"setval"`
+		}
+		err := conn.Select(&rows, `
+		SELECT
+			setval(
+				$1,
+				$2,
+				true
+			)
+		`, sequenceName, c.Value)
+		if err != nil {
+			return fmt.Errorf("updateSerialColumns: %w", err)
+		}
+		log.Printf("sequence set %s: %s", sequenceName, rows[0].SetVal)
+	}
+	return nil
 }
